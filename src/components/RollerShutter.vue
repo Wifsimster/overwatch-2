74eb7c1d1e802098c455757f08ecef99020a7327
Wifsimster <battistella@protonmail.com>
2021-08-02 10:01:22 +0200

    feat: roller shutter as component

--- src/components/RollerShutter.vue
+++ src/components/RollerShutter.vue
@@ -1,5 +1,5 @@
 <template>
-    <q-card>
+    <q-card v-if="device">
         <q-card-section class="column wrap items-center">
             <div class="text-h6">{{ device.name }}</div>
             <div class="text-subtitle1">{{ progress }}%</div>
@@ -54,11 +54,73 @@ import { defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'device',
-  props: [ 'device' ],
+  props: [ 'client', 'device' ],
   computed: {
     progress() {
       return Math.round(this.device.openedAt)
     }
   },
+  methods: {
+    stop() {
+      clearInterval(this.rollerShutter.interval)
+      if (this.rollerShutter.isOpening) {
+        this.rollerShutter.isOpening = false
+        this.client.publish(`cmnd/${this.rollerShutter.id}/Power1`, '0')
+      }
+      if (this.rollerShutter.isClosing) {
+        this.rollerShutter.isClosing = false
+        this.client.publish(`cmnd/${this.rollerShutter.id}/Power2`, '0')
+      }
+    },
+    open() {
+      if (this.rollerShutter.openedAt < 100 && !this.rollerShutter.isClosing) {
+        this.rollerShutter.isOpening = true
+
+        this.client.publish(`cmnd/${this.rollerShutter.id}/Power1`, '1')
+
+        this.rollerShutter.interval = setInterval(() => {
+          if(this.rollerShutter.openedAt < 100 - this.rollerShutter.ratio) {
+            this.rollerShutter.openedAt +=  this.rollerShutter.ratio
+          } else {
+            clearInterval(this.rollerShutter.interval)
+            this.rollerShutter.openedAt = 100
+            this.client.publish(`cmnd/${this.rollerShutter.id}/Power1`, '0')
+            this.rollerShutter.isOpening = false
+          }          
+        }, 1000)
+      }
+    },
+    close() {
+      if (this.rollerShutter.openedAt > 0 && !this.rollerShutter.isOpening) {
+        this.rollerShutter.isClosing = true
+
+        this.client.publish(`cmnd/${this.rollerShutter.id}/Power2`, '1')
+
+        this.rollerShutter.interval = setInterval(() => {
+          if(this.rollerShutter.openedAt > 0 + this.rollerShutter.ratio) {
+            this.rollerShutter.openedAt -= this.rollerShutter.ratio
+          } else {
+            clearInterval(this.rollerShutter.interval)
+            this.rollerShutter.openedAt = 0
+            this.client.publish(`cmnd/${this.rollerShutter.id}/Power2`, '0')
+            this.rollerShutter.isClosing = false
+          }
+        }, 1000)
+      }
+    },
+  },
+  watch: {
+    client() {
+      this.client.subscribe(`stat/${this.device.id}/Power`, (err) => {
+        if (err) {
+          console.error(err)
+          this.error = err
+        } else {
+          console.log(`Subscribe to stat/${this.device.id}/Power`)
+          this.client.publish(`cmnd/${this.device.id}/Power1`, '')
+        }
+      })
+    }
+  }
 })
 </script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -3,7 +3,7 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/Index.vue') },
+      { path: '', component: () => import('src/pages/Dashboard.vue') },
     ],
   },
 
